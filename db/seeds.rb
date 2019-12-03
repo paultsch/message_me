66996ffc9d276bb23413729ead32535aa87c9b8c
Paul Tschudin <paul.tschudin@gmail.com>
2019-12-02 21:14:34 -0600

    Add message partial and refractor some code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Chase", password: "password")
+User.create(username: "Kayla", password: "password")
+User.create(username: "Christen", password: "password")
+User.create(username: "Sophia", password: "password")
+User.create(username: "Teanna", password: "password")
